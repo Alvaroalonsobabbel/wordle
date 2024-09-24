a6999501664b07a3eedfca681dbf0e7da66c40ae
Alvaroalonsobabbel <aalonso@babbel.com>
2024-09-24 21:07:16 +0200

    refactored keyboard

--- pkg/terminal/keyboard_test.go
+++ pkg/terminal/keyboard_test.go
@@ -1,43 +1,41 @@
 package terminal
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Alvaroalonsobabbel/wordle/pkg/wordle"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestKeyboardUpdate(t *testing.T) {
-	result := &wordle.Result{wordle.Absent, wordle.Correct, wordle.Correct, wordle.Absent, wordle.Present}
+func TestKeyboardString(t *testing.T) {
+	wordle := wordle.NewGame(wordle.WithCustomWord("CHAIR"))
+	kb := NewKB(wordle)
 
-	kb := NewKB()
-	kb.update(result, "HELLO")
+	t.Run("game with no rounds return a basic keyboard", func(t *testing.T) {
+		expected := "   Q W E R T Z U I O P\n\r    A S D F G H J K L\n\r   ↩︎ Y X C V B N M ←\n\r"
+		assert.Equal(t, expected, kb.string())
+	})
 
-	assert.Equal(t, fmt.Sprintf(greyBackground, "H"), kb.am["H"])
-	assert.Equal(t, fmt.Sprintf(greenBackground, "E"), kb.am["E"])
-	assert.Equal(t, fmt.Sprintf(yellowBackground, "O"), kb.am["O"])
-
-	t.Run("correct letters get permanently marked as greenn", func(t *testing.T) {
-		result := &wordle.Result{wordle.Absent, wordle.Absent, wordle.Present, wordle.Absent, wordle.Absent}
-		kb.update(result, "STEAM")
-
-		assert.Equal(t, fmt.Sprintf(greenBackground, "E"), kb.am["E"])
+	t.Run("game with 1 round highlight the used letters", func(t *testing.T) {
+		wordle.Try("HELLO")
+		expected := "   Q W \x1b[7m\x1b[90mE\x1b[0m R T Z U I \x1b[7m\x1b[90mO\x1b[0m P\n\r    A S D F G \x1b[7m\x1b[33mH\x1b[0m J K \x1b[7m\x1b[90mL\x1b[0m\n\r   ↩︎ Y X C V B N M ←\n\r"
+		assert.Equal(t, expected, kb.string())
 	})
 }
 
-func TestKeyboardString(t *testing.T) {
-	kb := NewKB()
-	expected := "   Q W E R T Z U I O P\n\r    A S D F G H J K L\n\r   ← Y X C V B N M ↩︎\n\r"
-	assert.Equal(t, expected, kb.string())
-}
+func TestMapRunes(t *testing.T) {
+	w := wordle.NewGame(wordle.WithCustomWord("CHAIR"))
+	kb := NewKB(w)
 
-func TestGenerateAlphabet(t *testing.T) {
-	amap := newAlphabetMap()
+	word := "HELLO"
+	wantStatus := []int{wordle.Present, wordle.Absent, wordle.Absent, wordle.Absent, wordle.Absent}
+	w.Try(word)
 
-	assert.Equal(t, "A", amap["A"])
-	assert.Equal(t, "Z", amap["Z"])
+	kb.mapRunes()
 
-	_, ok := amap["Ä"]
-	assert.False(t, ok)
+	for i, v := range word {
+		rune, ok := kb.used[v]
+		assert.True(t, ok)
+		assert.Equal(t, wantStatus[i], rune)
+	}
 }
