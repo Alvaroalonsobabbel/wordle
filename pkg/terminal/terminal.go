e6336260a30fc7f4f2d6fcef5170569fab2b7177
Alvaroalonsobabbel <aalonso@babbel.com>
2024-09-28 13:32:02 +0200

    refactored terminal

--- pkg/terminal/terminal.go
+++ pkg/terminal/terminal.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"time"
 
 	"github.com/Alvaroalonsobabbel/wordle/pkg/wordle"
 )
@@ -16,10 +15,7 @@ import (
 const VERSION = "v0.1.1"
 
 const (
-	title    = "\033[1m6 attempts to find a 5-letter word\n\033[0m"
-	postGame = "(s)hare (e)xit"
-
-	// Relevant unicode characters.
+	// Relevant unicode characters to control the game.
 	enter     = 13
 	backspace = 127
 	ctrlC     = 3
@@ -27,212 +23,116 @@ const (
 
 	// Accepted characters regex.
 	okRegex = `^[A-Z\r\x7F]+$`
-
-	// Terminal formatting.
-	newLine          = "\n\r"
-	clearScreen      = "\033[H\033[2J"
-	green            = "\x1b[1m\x1b[32m%s\x1b[0m"
-	yellow           = "\x1b[1m\x1b[33m%s\x1b[0m"
-	black            = "\033[1m%s\033[0m"
-	greenBackground  = "\x1b[7m\x1b[32m%s\x1b[0m"
-	yellowBackground = "\x1b[7m\x1b[33m%s\x1b[0m"
-	greyBackground   = "\x1b[7m\x1b[90m%s\x1b[0m"
-	flash            = "\x1b[30m\x1b[47m%s\x1b[0m"
-	italics          = "\x1b[3m%s\x1b[0m"
 )
 
-type renderer struct {
-	wordle   *wordle.Game
-	keyboard *keyboard
-	rounds   []*round
-
-	printer  io.Writer
-	regex    *regexp.Regexp
-	errorMsg string
+type terminal struct {
+	wordle *wordle.Status
+	screen *screen
+	regex  *regexp.Regexp
+	reader io.Reader
 
 	buf []byte
 }
 
-func New(w io.Writer, hardMode bool) *renderer { //nolint: revive
-	return &renderer{
-		wordle:   wordle.NewGame(wordle.WithDalyWordle(), wordle.WithHardMode(hardMode)),
-		keyboard: NewKB(),
-		rounds:   NewRounds(),
-		printer:  w,
-		regex:    regexp.MustCompile(okRegex),
-		buf:      make([]byte, 1),
+func New(hardMode bool) *terminal { //nolint: revive
+	wordle := wordle.NewGame(wordle.WithDalyWordle(), wordle.WithHardMode(hardMode))
+	return &terminal{
+		reader: os.Stdin,
+		wordle: wordle,
+		screen: newScreen(wordle),
+		regex:  regexp.MustCompile(okRegex),
+		buf:    make([]byte, 1),
 	}
 }
 
-func NewTestTerminal(w io.Writer, word string) *renderer { //nolint: revive
-	return &renderer{
-		wordle:   wordle.NewGame(wordle.WithCustomWord(word)),
-		keyboard: NewKB(),
-		rounds:   NewRounds(),
-		printer:  w,
-		regex:    regexp.MustCompile(okRegex),
-		buf:      make([]byte, 1),
+func NewTestTerminal(w io.Writer, r io.Reader) *terminal { //nolint: revive
+	wordle := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+	return &terminal{
+		reader: r,
+		wordle: wordle,
+		screen: newTestScreen(w, wordle),
+		regex:  regexp.MustCompile(okRegex),
+		buf:    make([]byte, 1),
 	}
 }
 
-func (r *renderer) Start() {
-	r.render()
+func (t *terminal) Start() {
+	t.screen.renderAll()
+	defer t.screen.renderAll()
 
 	for {
-		ok, msg := r.wordle.Finish()
+		ok, msg := t.wordle.Finish()
 		if ok {
-			r.errorMsg = fmt.Sprintf(italics, msg)
-			r.render()
-			fmt.Fprint(r.printer, newLine+postGame)
-			r.postGame()
+			t.screen.msg = fmt.Sprintf(italics, msg)
+			t.screen.renderMsg()
+			t.postGame()
 
-			return
+			break
 		}
 
-		if quit := r.read(); quit {
-			return
+		if quit := t.read(); quit {
+			break
 		}
 
-		r.enter(r.buf[0])
+		t.processInput(t.buf[0])
 	}
 }
 
-func (r *renderer) postGame() {
+func (t *terminal) postGame() {
+	t.screen.renderPostGame()
 	for {
-		if quit := r.read(); quit {
+		if quit := t.read(); quit {
 			return
 		}
 
-		switch r.buf[0] {
+		switch t.buf[0] {
 		case 's', 'S':
-			r.render()
-			fmt.Fprint(r.printer, newLine+r.wordle.Share())
-			fmt.Fprint(r.printer, newLine+postGame)
+			t.screen.postGame = t.wordle.Share()
+			t.screen.renderPostGame()
 		case 'e', 'E':
-			fmt.Fprint(r.printer, newLine)
 			return
 		}
 	}
 }
 
-func (r *renderer) render() {
-	fmt.Fprint(r.printer, clearScreen)
-	fmt.Fprint(r.printer, title)
-	fmt.Fprint(r.printer, newLine)
-
-	for _, v := range r.rounds {
-		fmt.Fprint(r.printer, v.string())
-		fmt.Fprint(r.printer, newLine)
-	}
-
-	fmt.Fprint(r.printer, newLine)
-	fmt.Fprint(r.printer, r.errorMsg)
-	fmt.Fprint(r.printer, newLine)
-	fmt.Fprint(r.printer, r.keyboard.string())
-}
-
-func (r *renderer) enter(b byte) {
-	if !r.regex.MatchString(strings.ToUpper(string(b))) {
+func (t *terminal) processInput(b byte) {
+	if !t.regex.MatchString(strings.ToUpper(string(b))) {
 		return
 	}
 
-	r.showKBFlash(b)
+	t.screen.renderKBFlash(b)
 	switch b {
 	case backspace:
-		r.rounds[r.wordle.Round].backspace()
+		t.screen.rounds.backspace()
 	case enter:
-		if r.rounds[r.wordle.Round].index < 5 {
-			r.showError(errors.New("Not enough letters")) //nolint: stylecheck
+		if t.screen.rounds.all[t.wordle.Round].index < 5 {
+			t.screen.renderErr(errors.New("Not enough letters")) //nolint: stylecheck
 			return
 		}
 
-		lastWord := strings.Join(r.rounds[r.wordle.Round].status, "")
-		if err := r.wordle.Try(lastWord); err != nil {
-			r.showError(err)
+		lastWord := strings.Join(t.screen.rounds.all[t.wordle.Round].status, "")
+		if err := t.wordle.Try(lastWord); err != nil {
+			t.screen.renderErr(err)
 			return
 		}
 
-		r.showResult()
-
-		// keyboard.update happens after wordle.Try increments the
-		// round counter. that's why we send the previous round's result.
-		r.keyboard.update(&r.wordle.Results[r.wordle.Round-1], lastWord)
+		t.screen.renderResult()
+		t.screen.renderKB()
 	default:
-		r.rounds[r.wordle.Round].add(string(b))
-	}
-	r.render()
-}
-
-func (r *renderer) showResult() {
-	// showResult happens after wordle.Try increments the round counter.
-	// that's why we use the round before here.
-	c := r.wordle.Round - 1
-
-	for i, v := range r.rounds[c].status {
-		color := black
-		switch r.wordle.Results[c][i] {
-		case wordle.Correct:
-			color = green
-		case wordle.Present:
-			color = yellow
-		}
-
-		r.rounds[c].status[i] = "_"
-		r.render()
-		time.Sleep(150 * time.Millisecond)
-		r.rounds[c].status[i] = fmt.Sprintf(color, v)
-	}
-}
-
-func (r *renderer) showError(err error) {
-	go func() {
-		r.errorMsg = fmt.Sprintf(italics, err.Error())
-		defer func() {
-			r.errorMsg = ""
-			r.render()
-		}()
-
-		for i := range 6 {
-			if i%2 == 0 {
-				r.rounds[r.wordle.Round].animation = " "
-			} else {
-				r.rounds[r.wordle.Round].animation = ""
-			}
-			r.render()
-			time.Sleep(50 * time.Millisecond)
-		}
-		time.Sleep(1500 * time.Millisecond)
-	}()
-}
-
-func (r *renderer) showKBFlash(l byte) {
-	var char string
-	switch l {
-	case backspace:
-		char = "←"
-	case enter:
-		char = "↩︎"
-	default:
-		char = strings.ToUpper(string(l))
+		t.screen.rounds.add(string(b))
 	}
 
-	f := fmt.Sprintf(flash, char)
-	temp := r.keyboard.am[char]
-	r.keyboard.am[char] = f
-	r.render()
-	r.keyboard.am[char] = temp
-	time.Sleep(25 * time.Millisecond)
-	r.render()
+	t.screen.renderRound()
 }
 
-func (r *renderer) read() bool {
-	_, err := os.Stdin.Read(r.buf)
+func (t *terminal) read() bool {
+	_, err := t.reader.Read(t.buf)
 	if err != nil {
 		log.Fatalf("Error reading input: %v", err)
 	}
 
 	// Ctrl-C or Esc exits the game
-	if r.buf[0] == ctrlC || r.buf[0] == esc {
+	if t.buf[0] == ctrlC || t.buf[0] == esc {
 		return true
 	}
 
--- pkg/terminal/terminal_test.go
+++ pkg/terminal/terminal_test.go
@@ -1,84 +1,45 @@
 package terminal
 
 import (
-	"bytes"
 	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRender(t *testing.T) {
-	var buffer bytes.Buffer
-	terminal := NewTestTerminal(&buffer, "HELLO")
-
-	terminal.render()
-	result := buffer.String()
-	expectedResult := "\x1b[H\x1b[2J\x1b[1m6 attempts to find a 5-letter word\n\x1b[0m\n\r\t_ _ _ _ _\n\r\t_ _ _ _ _\n\r\t_ _ _ _ _\n\r\t_ _ _ _ _\n\r\t_ _ _ _ _\n\r\t_ _ _ _ _\n\r\n\r\n\r   Q W E R T Z U I O P\n\r    A S D F G H J K L\n\r   ← Y X C V B N M ↩︎\n\r"
-	assert.Equal(t, expectedResult, result)
+type mockReader struct {
+	data []byte
 }
 
-func TestEnter(t *testing.T) {
-	var buffer = io.Discard
-	t.Run("two letters on first round", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "HELLO")
+func (m *mockReader) Read(p []byte) (int, error) {
+	n := copy(p, m.data)
+	m.data = m.data[n:]
+	return len(p), nil
+}
 
-		terminal.enter(97) // A
-		want := "\tA _ _ _ _"
-		assert.Equal(t, want, terminal.rounds[0].string())
+func TestRead(t *testing.T) {
+	mockReader := &mockReader{}
+	terminal := NewTestTerminal(io.Discard, mockReader)
 
-		terminal.enter(98) // B
-		want = "\tA B _ _ _"
-		assert.Equal(t, want, terminal.rounds[0].string())
-	})
+	tests := []struct {
+		name     string
+		input    []byte
+		wantExit bool
+	}{
+		{"ctrl-c exits the game", []byte{ctrlC}, true},
+		{"esc exits the game", []byte{esc}, true},
+		{"'A' does not exits the game", []byte{'A'}, false},
+	}
 
-	t.Run("passing an entire round adds colors to the words", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "CHORE")
-		terminal.enter(99)  // C
-		terminal.enter(104) // H
-		terminal.enter(97)  // A
-		terminal.enter(114) // R
-		terminal.enter(109) // M
-		terminal.enter(enter)
-		terminal.enter(99) // C
-		want := "\t\x1b[1m\x1b[32mC\x1b[0m \x1b[1m\x1b[32mH\x1b[0m \x1b[1mA\x1b[0m \x1b[1m\x1b[32mR\x1b[0m \x1b[1mM\x1b[0m"
-		assert.Equal(t, want, terminal.rounds[0].string())
-		want = "\tC _ _ _ _"
-		assert.Equal(t, want, terminal.rounds[1].string())
-	})
-
-	t.Run("backspace returns one space", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "CHORE")
-		terminal.enter(99)  // C
-		terminal.enter(104) // H
-		terminal.enter(backspace)
-		terminal.enter(97) // A
-
-		want := "\tC A _ _ _"
-		assert.Equal(t, want, terminal.rounds[0].string())
-	})
-
-	t.Run("backspace on the first line does nothing", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "CHORE")
-		terminal.enter(backspace)
-		want := "\t_ _ _ _ _"
-		assert.Equal(t, want, terminal.rounds[0].string())
-	})
-
-	t.Run("writing further than 5 letters does nothing", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "CHORE")
-		for range 6 {
-			terminal.enter(97) // A
-		}
-		want := "\tA A A A A"
-		assert.Equal(t, want, terminal.rounds[0].string())
-	})
-
-	t.Run("not allowed letters are ignored", func(t *testing.T) {
-		terminal := NewTestTerminal(buffer, "CHORE")
-		terminal.enter(49) // 1
-
-		want := "\t_ _ _ _ _"
-		assert.Equal(t, want, terminal.rounds[0].string())
-	})
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockReader.data = test.input
+			exit := terminal.read()
+			if test.wantExit {
+				assert.True(t, exit)
+			} else {
+				assert.False(t, exit)
+			}
+		})
+	}
 }
