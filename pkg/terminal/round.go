ca7fa021c8e014b6228c5263f50001460711fbfa
Alvaroalonsobabbel <aalonso@babbel.com>
2024-10-13 12:12:06 +0200

    refactor round

--- pkg/terminal/round.go
+++ pkg/terminal/round.go
@@ -2,39 +2,38 @@ package terminal
 
 import (
 	"fmt"
-	"strings"
+	"time"
 
 	"github.com/Alvaroalonsobabbel/wordle/pkg/wordle"
+)
+
+const (
+	moveToYX    = "\033[%d;%dH%s"
+	roundPos    = "\033[%d;9H"
+	roundOffset = 3
 )
 
 type round struct {
 	index     int
 	status    []string
 	animation string
+	wordle    *wordle.Status
+	render    *render
 }
 
-type rounds struct {
-	w   *wordle.Status
-	all []round
-}
-
-func newRounds(w *wordle.Status) *rounds { //nolint: revive
-	var r []round
-	for range 6 {
-		r = append(r, round{status: strings.Split(strings.Repeat("_", 5), "")})
-	}
-	return &rounds{
-		w:   w,
-		all: r,
+func newRound(w *wordle.Status, r *render) *round { //nolint: revive
+	return &round{
+		render: r,
+		wordle: w,
+		status: []string{"_", "_", "_", "_", "_"},
 	}
 }
 
-func (r *rounds) string(round int) string {
-	p := tab
+func (r *round) print(round int) {
+	p := fmt.Sprintf(roundPos, round+roundOffset)
 
-	if round < len(r.w.Results) {
-		var str []string
-		for _, b := range r.w.Results[round] {
+	if round < len(r.wordle.Results) {
+		for _, b := range r.wordle.Results[round] {
 			for k, v := range b {
 				var color string
 				switch v {
@@ -45,34 +44,86 @@ func (r *rounds) string(round int) string {
 				case wordle.Absent:
 					color = greyBackground
 				}
-				str = append(str, fmt.Sprintf(color, string(k)))
+				p += fmt.Sprintf(color, string(k))
 			}
 		}
-		p += strings.Join(str, "")
-	} else if r.w.Round < 6 {
-		p += r.all[round].animation
-		for _, s := range r.all[round].status {
+	} else if r.wordle.Round < 6 {
+		p += r.animation
+		for _, s := range r.status {
 			p += fmt.Sprintf(emptyChar, s)
 		}
 	}
 
-	return p
+	r.render.string(p + " ") // Trailing space to delete the error animation tail.
 }
 
-func (r *rounds) add(s string) {
-	if r.all[r.w.Round].index == 5 {
+func (r *round) shake() {
+	go func() {
+		for i := range 6 {
+			if i%2 == 0 {
+				r.animation = " "
+			} else {
+				r.animation = ""
+			}
+
+			r.print(r.wordle.Round)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}()
+}
+
+func (r *round) renderResult() {
+	defer r.reset()
+
+	var (
+		// results are displyed after wordle.Try increments the internal
+		// round counter that's why we run it with wordle.Round - 1
+		round = r.wordle.Round - 1
+		row   = round + roundOffset
+	)
+
+	for i, res := range r.wordle.Results[round] {
+		for k, v := range res {
+			color := greyBackground
+			switch v {
+			case wordle.Correct:
+				color = greenBackground
+			case wordle.Present:
+				color = yellowBackground
+			}
+
+			// 9 is the distance between the marging and the round.
+			// 3 is the spaces each letter occupies.
+			r.render.string(fmt.Sprintf(moveToYX, row, 9+(i*3), " _ "))
+			time.Sleep(250 * time.Millisecond)
+			r.render.string(fmt.Sprintf(moveToYX, row, 9+(i*3), fmt.Sprintf(color, string(k))))
+		}
+	}
+}
+
+func (r *round) add(s string) {
+	defer r.print(r.wordle.Round)
+
+	if r.index == 5 {
 		return
 	}
 
-	r.all[r.w.Round].status[r.all[r.w.Round].index] = strings.ToUpper(s)
-	r.all[r.w.Round].index++
+	r.status[r.index] = s
+	r.index++
 }
 
-func (r *rounds) backspace() {
-	if r.all[r.w.Round].index == 0 {
+func (r *round) backspace() {
+	defer r.print(r.wordle.Round)
+
+	if r.index == 0 {
 		return
 	}
 
-	r.all[r.w.Round].index--
-	r.all[r.w.Round].status[r.all[r.w.Round].index] = "_"
+	r.index--
+	r.status[r.index] = "_"
+}
+
+func (r *round) reset() {
+	r.index = 0
+	r.status = []string{"_", "_", "_", "_", "_"}
 }
--- pkg/terminal/round_test.go
+++ pkg/terminal/round_test.go
@@ -1,103 +1,122 @@
 package terminal
 
 import (
+	"bytes"
+	"io"
 	"testing"
 
 	"github.com/Alvaroalonsobabbel/wordle/pkg/wordle"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRoundString(t *testing.T) {
+func TestRoundPrint(t *testing.T) {
 	wordle := wordle.NewGame(wordle.WithCustomWord("CHORE"))
-	rounds := newRounds(wordle)
+	buf := &bytes.Buffer{}
+	render, _ := newRender(buf)
+	round := newRound(wordle, render)
 
 	t.Run("emtpy rounds", func(t *testing.T) {
-		want := "\t _  _  _  _  _ "
-		assert.Equal(t, want, rounds.string(0))
+		round.print(0)
+		render.wg.Wait()
+		want := "\x1b[3;9H _  _  _  _  _  "
+		assert.Equal(t, want, buf.String())
 	})
 
 	t.Run("with one letter", func(t *testing.T) {
-		rounds.add("A")
-		want := "\t A  _  _  _  _ "
-		assert.Equal(t, want, rounds.string(0))
+		buf.Reset()
+		round.add("A")
+		render.wg.Wait()
+		want := "\x1b[3;9H A  _  _  _  _  "
+		assert.Equal(t, want, buf.String())
 	})
 
 	t.Run("with two letters and animations space", func(t *testing.T) {
-		rounds.add("A")
-		rounds.all[0].animation = " "
-		want := "\t  A  A  _  _  _ "
-		assert.Equal(t, want, rounds.string(0))
+		buf.Reset()
+		round.add("A")
+		render.wg.Wait()
+		want := "\x1b[3;9H A  A  _  _  _  "
+		assert.Equal(t, want, buf.String())
 	})
 
 	t.Run("after a round exist in wordle status it prints the status with color", func(t *testing.T) {
+		buf.Reset()
 		wordle.Try("SCORE") //nolint: errcheck
-		want := "\t\x1b[7m\x1b[90m S \x1b[0m\x1b[7m\x1b[33m C \x1b[0m\x1b[7m\x1b[32m O \x1b[0m\x1b[7m\x1b[32m R \x1b[0m\x1b[7m\x1b[32m E \x1b[0m"
-		assert.Equal(t, want, rounds.string(0))
-		want = "\t _  _  _  _  _ "
-		assert.Equal(t, want, rounds.string(1))
+		round.print(0)
+		render.wg.Wait()
+		want := "\x1b[3;9H\x1b[7m\x1b[90m S \x1b[0m\x1b[7m\x1b[33m C \x1b[0m\x1b[7m\x1b[32m O \x1b[0m\x1b[7m\x1b[32m R \x1b[0m\x1b[7m\x1b[32m E \x1b[0m "
+		assert.Equal(t, want, buf.String())
+
+		buf.Reset()
+		round.reset()
+		round.print(1)
+		render.wg.Wait()
+		want = "\x1b[4;9H _  _  _  _  _  "
+		assert.Equal(t, want, buf.String())
 	})
 }
 
 func TestAdd(t *testing.T) {
 	t.Run("adding one letter", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
+		wordle := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+		render, _ := newRender(io.Discard)
+		round := newRound(wordle, render)
 
-		rounds.add("A")
-
-		assert.Equal(t, "A", rounds.all[0].status[0])
+		round.add("A")
+		assert.Equal(t, "A", round.status[0])
 	})
 
 	t.Run("adding five consecutive letters", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
+		wordle := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+		render, _ := newRender(io.Discard)
+		round := newRound(wordle, render)
 		letters := []string{"A", "B", "C", "D", "E"}
 
 		for _, l := range letters {
-			rounds.add(l)
+			round.add(l)
 		}
 
 		for i := range letters {
-			assert.Equal(t, letters[i], rounds.all[0].status[i])
+			assert.Equal(t, letters[i], round.status[i])
 		}
 	})
 
 	t.Run("adding more than 5 letters does not increment the counter nor adds another letter", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
+		wordle := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+		render, _ := newRender(io.Discard)
+		round := newRound(wordle, render)
 		letters := []string{"A", "B", "C", "D", "E", "F"}
 
 		for _, l := range letters {
-			rounds.add(l)
+			round.add(l)
 		}
 
-		rounds.add("A")
+		round.add("A")
 
-		assert.Equal(t, "E", rounds.all[0].status[4])
-		assert.Equal(t, 5, rounds.all[0].index)
-	})
-
-	t.Run("adding a lower case letter makes it upper case", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
-		rounds.add("a")
-
-		assert.Equal(t, "A", rounds.all[0].status[0])
+		assert.Equal(t, "E", round.status[4])
+		assert.Equal(t, 5, round.index)
 	})
 }
 
 func TestBackspace(t *testing.T) {
 	t.Run("reverts the counter and replaces the letter with underscore", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
-		rounds.add("A")
-		rounds.add("B")
-		rounds.backspace()
+		w := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+		r, _ := newRender(io.Discard)
+		round := newRound(w, r)
+		round.add("A")
+		round.add("B")
+		round.backspace()
 
-		assert.Equal(t, "_", rounds.all[0].status[1])
-		assert.Equal(t, 1, rounds.all[0].index)
+		assert.Equal(t, "_", round.status[1])
+		assert.Equal(t, 1, round.index)
 	})
 
 	t.Run("when counter is 0, backspace has no effect", func(t *testing.T) {
-		rounds := newRounds(wordle.NewGame(wordle.WithCustomWord("CHORE")))
-		rounds.backspace()
+		w := wordle.NewGame(wordle.WithCustomWord("CHORE"))
+		r, _ := newRender(io.Discard)
+		round := newRound(w, r)
+		round.backspace()
 
-		assert.Equal(t, "_", rounds.all[0].status[0])
-		assert.Equal(t, 0, rounds.all[0].index)
+		assert.Equal(t, "_", round.status[0])
+		assert.Equal(t, 0, round.index)
 	})
 }
