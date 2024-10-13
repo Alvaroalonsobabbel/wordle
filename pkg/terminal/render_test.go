ad19b954a1d7cdfdda3549653a3cee14bc83611a
Alvaroalonsobabbel <aalonso@babbel.com>
2024-10-13 11:30:07 +0200

    added render

---
+++ pkg/terminal/render_test.go
@@ -0,0 +1,30 @@
+package terminal
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRender(t *testing.T) {
+	t.Run("prints formatted err to w and clears error after", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		render, _ := newRender(buf)
+		render.dur = 10 * time.Millisecond
+
+		render.err("123")
+		render.wg.Wait()
+		assert.Equal(t, "\x1b[3;28H\x1b[K\x1b[4;28H\x1b[K\x1b[3;28H\x1b[3m\x1b[30m\x1b[47m 123 \x1b[0m\x1b[3;28H\x1b[K", buf.String())
+	})
+
+	t.Run("prints str to w", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		render, _ := newRender(buf)
+
+		render.string("123")
+		render.wg.Wait()
+		assert.Equal(t, "123", buf.String())
+	})
+}
