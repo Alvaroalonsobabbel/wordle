8cecd28f78d8cf4eccebdd92367e98bcf698efb7
Alvaroalonsobabbel <aalonso@babbel.com>
2024-10-26 17:19:32 +0200

    simplified render err queue

--- pkg/terminal/render_test.go
+++ pkg/terminal/render_test.go
@@ -12,11 +12,13 @@ func TestRender(t *testing.T) {
 	t.Run("prints formatted err to w and clears error after", func(t *testing.T) {
 		buf := &bytes.Buffer{}
 		render := newRender(buf)
-		render.dur = 10 * time.Millisecond
+		render.errDur = 10 * time.Millisecond
 
 		render.err("123")
 		render.wg.Wait()
-		assert.Equal(t, "\x1b[3;28H\x1b[K\x1b[4;28H\x1b[K\x1b[3;28H\x1b[3m\x1b[30m\x1b[47m 123 \x1b[0m\x1b[3;28H\x1b[K", buf.String())
+
+		want := "\x1b[3;28H\x1b[K\x1b[4;28H\x1b[K\x1b[3;28H\x1b[3m\x1b[30m\x1b[47m 123 \x1b[0m\x1b[3;28H\x1b[K"
+		assert.Equal(t, want, buf.String())
 	})
 
 	t.Run("prints str to w", func(t *testing.T) {
@@ -24,7 +26,7 @@ func TestRender(t *testing.T) {
 		render := newRender(buf)
 
 		render.string("123")
-		render.wg.Wait()
+		// render.wg.Wait()
 		assert.Equal(t, "123", buf.String())
 	})
 }
