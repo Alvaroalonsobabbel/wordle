5e57685bdd8d8f20164e62e6bbafee2df2dfccea
Alvaroalonsobabbel <aalonso@babbel.com>
2024-12-20 16:57:04 +0100

    fix: keyboard color bug

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/Alvaroalonsobabbel/wordle/wordle"
 )
 
-const VERSION = "v0.4.7"
+const VERSION = "v0.4.8"
 
 const (
 	hardModeFlag     = "hard"
--- terminal/keyboard.go
+++ terminal/keyboard.go
@@ -10,70 +10,72 @@ import (
 
 const flash = "\x1b[30m\x1b[47m %s \x1b[0m"
 
+type key struct {
+	value  string
+	column int
+	row    int
+}
+
+func (k *key) string() string { //nolint: revive
+	return fmt.Sprintf("\x1b[%d;%dH%s", k.row, k.column, k.value)
+}
+
 type keyboard struct {
-	wordle    *wordle.Status
-	render    *render
-	used      map[rune]int
-	flashChar string
+	keys   map[string]*key
+	wordle *wordle.Status
+	render *render
 }
 
 func newKeyboard(w *wordle.Status, r *render) *keyboard { //nolint: revive
 	return &keyboard{
 		wordle: w,
 		render: r,
-		used:   make(map[rune]int),
+		keys: map[string]*key{
+			"Q":  {" Q ", 2, 11},
+			"W":  {" W ", 5, 11},
+			"E":  {" E ", 8, 11},
+			"R":  {" R ", 11, 11},
+			"T":  {" T ", 14, 11},
+			"Z":  {" Z ", 17, 11},
+			"U":  {" U ", 20, 11},
+			"I":  {" I ", 23, 11},
+			"O":  {" O ", 26, 11},
+			"P":  {" P ", 29, 11},
+			"A":  {" A ", 3, 12},
+			"S":  {" S ", 6, 12},
+			"D":  {" D ", 9, 12},
+			"F":  {" F ", 12, 12},
+			"G":  {" G ", 15, 12},
+			"H":  {" H ", 18, 12},
+			"J":  {" J ", 21, 12},
+			"K":  {" K ", 24, 12},
+			"L":  {" L ", 27, 12},
+			"↩︎": {" ↩︎ ", 2, 13},
+			"Y":  {" Y ", 5, 13},
+			"X":  {" X ", 8, 13},
+			"C":  {" C ", 11, 13},
+			"V":  {" V ", 14, 13},
+			"B":  {" B ", 17, 13},
+			"N":  {" N ", 20, 13},
+			"M":  {" M ", 23, 13},
+			"←":  {" ← ", 26, 13},
+		},
 	}
 }
 
 func (kb *keyboard) print() {
-	kb.mapRunes()
-
-	var (
-		firstRow  = []string{"Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P"}
-		secondRow = []string{"A", "S", "D", "F", "G", "H", "J", "K", "L"}
-		thirdRow  = []string{"↩︎", "Y", "X", "C", "V", "B", "N", "M", "←"}
-	)
-
-	kb.render.string("\033[11;2H" + kb.renderRow(firstRow))
-	kb.render.string("\033[12;3H" + kb.renderRow(secondRow))
-	kb.render.string("\033[13;2H" + kb.renderRow(thirdRow))
-}
-
-func (kb *keyboard) renderRow(row []string) string {
-	for i, v := range row {
-		if v == kb.flashChar {
-			row[i] = fmt.Sprintf(flash, v)
-		} else {
-			row[i] = fmt.Sprintf(emptyChar, v)
+	for v, k := range kb.keys {
+		if strings.Contains(string(kb.wordle.Used), v) {
+			k.value = fmt.Sprintf(greyBackground, v)
+		}
+		if strings.Contains(string(kb.wordle.Hints), v) {
+			k.value = fmt.Sprintf(yellowBackground, v)
+		}
+		if strings.Contains(string(kb.wordle.Discovered[:]), v) {
+			k.value = fmt.Sprintf(greenBackground, v)
 		}
 
-		stat, ok := kb.used[[]rune(v)[0]]
-		if ok {
-			var c string
-			switch stat {
-			case wordle.Correct:
-				c = greenBackground
-			case wordle.Present:
-				c = yellowBackground
-			case wordle.Absent:
-				c = greyBackground
-			}
-			row[i] = fmt.Sprintf(c, v)
-		}
-	}
-
-	return strings.Join(row, "")
-}
-
-func (kb *keyboard) mapRunes() {
-	for _, all := range kb.wordle.Results {
-		for _, round := range all {
-			for k, v := range round {
-				if kb.used[k] != wordle.Correct {
-					kb.used[k] = v
-				}
-			}
-		}
+		kb.render.string(k.string())
 	}
 }
 
@@ -88,9 +90,16 @@ func (kb *keyboard) flash(l byte) {
 		char = strings.ToUpper(string(l))
 	}
 
-	kb.flashChar = char
-	kb.print()
-	time.Sleep(25 * time.Millisecond)
-	kb.flashChar = ""
-	kb.print()
+	key, ok := kb.keys[char]
+	if !ok {
+		return
+	}
+
+	oldValue := key.value
+	key.value = fmt.Sprintf(flash, char)
+	kb.render.string(key.string())
+	time.AfterFunc(25*time.Millisecond, func() {
+		key.value = oldValue
+		kb.render.string(key.string())
+	})
 }
